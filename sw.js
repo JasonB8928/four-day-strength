ef617ea95fdec630ce8757ebf0116b07f9091666
JasonB8928 <jasonfboyce@yahoo.com.au>
2026-09-18 14:26:07 +0800

    Version 3.1 - settings toggle fix.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'four-day-strength-v3';
+const CACHE = 'four-day-strength-v3-1';
 const ASSETS = ['./', './index.html', './manifest.webmanifest'];
 self.addEventListener('install', e => {
   self.skipWaiting();
